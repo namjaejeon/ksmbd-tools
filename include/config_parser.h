bca5149b9110171ef271371b1275e7192348cad0
Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
2018-04-05 18:30:01 +0900

    update linux-cifsd-devel mailing list address
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>

--- include/config_parser.h
+++ include/config_parser.h
@@ -1,7 +1,7 @@
 /*
  *   Copyright (C) 2018 Samsung Electronics Co., Ltd.
  *
- *   linux-cifsd-devel@sourceforge.net
+ *   linux-cifsd-devel@lists.sourceforge.net
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
