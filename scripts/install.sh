8dde3c6bf5cc16544d87452be8c4af904289f869
Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
2018-08-20 19:15:14 +0900

    cifsd-tools: add a trivial usage printout to systemd service installation script
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,3 +3,6 @@
 mkdir -p ~/.config/systemd/user
 cp cifsd.service ~/.config/systemd/user
 systemctl --user daemon-reload
+
+echo "Run 'systemctl --user start cifsd.service' to start the service"
+echo "Run 'systemctl --user start cifsd.service' to stop the service"
