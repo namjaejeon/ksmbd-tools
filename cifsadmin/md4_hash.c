8d8a0138f2701f115125df323832a8027bb04195
Namjae Jeon <namjae.jeon@protocolfreedom.org>
2018-09-27 15:45:02 +0900

    cifsd-tools: use SPDX license identifiers
    Use SPDX license identiefiers to cifsd-tools files and remove GPL 2.0
    license boilerplate.
    
    Signed-off-by: Namjae Jeon <namjae.jeon@protocolfreedom.org>

--- cifsadmin/md4_hash.c
+++ cifsadmin/md4_hash.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Cryptographic API.
  *
@@ -14,12 +15,6 @@
  * Copyright (c) Cryptoapi developers.
  * Copyright (c) 2002 David S. Miller (davem@redhat.com)
  * Copyright (c) 2002 James Morris <jmorris@intercode.com.au>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
  */
 
 #include <stdlib.h>
