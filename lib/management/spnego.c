ab1d31ec7b0d4088982b7c216239f90505affdc1
Hyunchul Lee <hyc.lee@gmail.com>
2020-11-22 14:40:57 +0900

    ksmbd-tools: implement decoding SPNEGO-based Kerberos
    Decode negTokenInit and get mechToken and the
    mechanism type.
    
    Signed-off-by: Hyunchul Lee <hyc.lee@gmail.com>

--- lib/management/spnego.c
+++ lib/management/spnego.c
@@ -21,6 +21,7 @@
 
 #include <linux/ksmbd_server.h>
 #include <management/spnego.h>
+#include <asn1.h>
 #include "spnego_mech.h"
 
 static struct spnego_mech_ctx mech_ctxs[SPNEGO_MAX_MECHS];
@@ -64,10 +65,138 @@ void spnego_destroy(void)
 	}
 }
 
+static int compare_oid(unsigned long *oid1, unsigned int oid1len,
+		    unsigned long *oid2, unsigned int oid2len)
+{
+	unsigned int i;
+
+	if (oid1len != oid2len)
+		return 1;
+
+	for (i = 0; i < oid1len; i++) {
+		if (oid1[i] != oid2[i])
+			return 1;
+	}
+	return 0;
+}
+
+static bool is_supported_mech(unsigned long *oid, unsigned int len,
+			int *mech_type)
+{
+	*mech_type = SPNEGO_MAX_MECHS;
+	return false;
+}
+
+static int decode_asn1_header(struct asn1_ctx *ctx, unsigned char **end,
+		unsigned int cls, unsigned int con, unsigned int tag)
+{
+	unsigned int d_cls, d_con, d_tag;
+
+	if (asn1_header_decode(ctx, end, &d_cls, &d_con, &d_tag) == 0 ||
+		(d_cls != cls || d_con != con || d_tag != tag))
+		return -EINVAL;
+	return 0;
+}
+
 static int decode_negTokenInit(unsigned char *negToken, int token_len,
 			int *mech_type, unsigned char **krb5_ap_req,
 			unsigned int *req_len)
 {
+	struct asn1_ctx ctx;
+	unsigned char *end, *mech_types_end, *id;
+	unsigned long *oid = NULL;
+	unsigned int len;
+
+	asn1_open(&ctx, negToken, token_len);
+
+	/* GSSAPI header */
+	if (decode_asn1_header(&ctx, &end, ASN1_APL, ASN1_CON, ASN1_EOC)) {
+		pr_debug("Error decoding SPNEGO application tag\n");
+		return -EINVAL;
+	}
+
+	/* SPNEGO oid */
+	if (decode_asn1_header(&ctx, &end, ASN1_UNI, ASN1_PRI, ASN1_OJI) ||
+			asn1_oid_decode(&ctx, end, &oid, &len) == 0 ||
+			compare_oid(oid, len, SPNEGO_OID, SPNEGO_OID_LEN)) {
+		pr_debug("Error decoding SPNEGO oid\n");
+		free(oid);
+		return -EINVAL;
+	}
+	free(oid);
+
+	/* negoTokenInit */
+	if (decode_asn1_header(&ctx, &end, ASN1_CTX, ASN1_CON, 0) ||
+			decode_asn1_header(&ctx, &end,
+				ASN1_UNI, ASN1_CON, ASN1_SEQ)) {
+		pr_debug("Error decoding negTokenInit tag\n");
+		return -EINVAL;
+	}
+
+	/* mechTypes */
+	if (decode_asn1_header(&ctx, &end, ASN1_CTX, ASN1_CON, 0) ||
+			decode_asn1_header(&ctx, &end,
+				ASN1_UNI, ASN1_CON, ASN1_SEQ)) {
+		pr_debug("Error decoding mechTypes tag\n");
+		return -EINVAL;
+	}
+
+	mech_types_end = end;
+	if (decode_asn1_header(&ctx, &end, ASN1_UNI, ASN1_PRI, ASN1_OJI) ||
+			asn1_oid_decode(&ctx, end, &oid, &len) == 0) {
+		pr_debug("Error decoding Kerberos oids\n");
+		return -EINVAL;
+	}
+
+	if (!is_supported_mech(oid, len, mech_type)) {
+		free(oid);
+		pr_debug("Not support mechanism\n");
+		return -EINVAL;
+	}
+	free(oid);
+
+	ctx.pointer = mech_types_end;
+	/* mechToken */
+	if (decode_asn1_header(&ctx, &end, ASN1_CTX, ASN1_CON, 2) ||
+			decode_asn1_header(&ctx, &end,
+				ASN1_UNI, ASN1_PRI, ASN1_OTS)) {
+		pr_debug("Error decoding krb5_blob\n");
+		return -EINVAL;
+	}
+
+	if (decode_asn1_header(&ctx, &end, ASN1_APL, ASN1_CON, ASN1_EOC)) {
+		pr_debug("Error decoding GSSAPI application tag\n");
+		return -EINVAL;
+	}
+
+	/* Kerberos 5 oid */
+	if (decode_asn1_header(&ctx, &end, ASN1_UNI, ASN1_PRI, ASN1_OJI)) {
+		pr_debug("Error decoding Kerberos oid tag\n");
+		return -EINVAL;
+	}
+
+	if (asn1_oid_decode(&ctx, end, &oid, &len) == 0 ||
+			compare_oid(oid, len, KRB5_OID,
+				ARRAY_SIZE(KRB5_OID))) {
+		pr_debug("not Kerberos OID\n");
+		free(oid);
+		return -EINVAL;
+	}
+	free(oid);
+
+	/* AP_REQ id */
+	if (asn1_read(&ctx, &id, 2) == 0 || id[0] != 1 || id[1] != 0) {
+		if (id)
+			free(id);
+		pr_debug("Error decoding AP_REQ id\n");
+		return -EINVAL;
+	}
+	free(id);
+
+	/* AP_REQ */
+	*req_len = (unsigned int)(ctx.end - ctx.pointer);
+	*krb5_ap_req = ctx.pointer;
+	return 0;
 }
 
 static int encode_negTokenTarg(char *in_blob, int in_len,
