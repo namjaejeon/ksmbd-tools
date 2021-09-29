04d2a45bde7b669b94e2f0e7e9824e7c52400286
Enzo Matsumiya <ematsumiya@suse.de>
2021-09-29 15:39:13 -0300

    control: clarify some error messages
    And also change some logic in return values that were not really
    necessary/used.
    
    Signed-off-by: Enzo Matsumiya <ematsumiya@suse.de>

--- control/control.c
+++ control/control.c
@@ -37,53 +37,63 @@ static int ksmbd_control_shutdown(void)
 
 	fd = open("/sys/class/ksmbd-control/kill_server", O_WRONLY);
 	if (fd < 0) {
-		pr_err("open failed: %d\n", errno);
+		pr_err("open failed (%d), is ksmbd.ko loaded?\n", errno);
 		return fd;
 	}
 
 	ret = write(fd, "hard", 4);
 	close(fd);
+
+	if (ret != 4)
+		pr_err("failed to send shutdown to ksmbd module\n");
+
 	return ret;
 }
 
-static int ksmbd_control_show_version(void)
+static void ksmbd_control_show_version(void)
 {
 	int fd, ret;
 	char ver[255] = {0};
 
 	fd = open("/sys/module/ksmbd/version", O_RDONLY);
 	if (fd < 0) {
-		pr_err("open failed: %d\n", errno);
-		return fd;
+		pr_err("open failed (%d), is ksmbd.ko loaded?\n", errno);
+		return;
 	}
 
 	ret = read(fd, ver, 255);
 	close(fd);
+
 	if (ret != -1)
 		pr_info("ksmbd version : %s\n", ver);
-	return ret;
+	else
+		pr_err("error getting ksmbd version from module\n");
 }
 
-static int ksmbd_control_debug(char *comp)
+static int ksmbd_control_debug(char *type)
 {
 	int fd, ret;
 	char buf[255] = {0};
 
 	fd = open("/sys/class/ksmbd-control/debug", O_RDWR);
 	if (fd < 0) {
-		pr_err("open failed: %d\n", errno);
+		pr_err("open failed (%d), is ksmbd.ko loaded?\n", errno);
 		return fd;
 	}
 
-	ret = write(fd, comp, strlen(comp));
+	ret = write(fd, type, strlen(type));
 	if (ret < 0)
 		goto out;
 	ret = read(fd, buf, 255);
 	if (ret < 0)
 		goto out;
 
-	pr_info("%s\n", buf);
 out:
+	if (ret < 0)
+		pr_err("read/write failed: %d\n", errno);
+	else
+		pr_info("%s\n", buf);
+
 	close(fd);
 	return ret;
 }
@@ -93,10 +103,13 @@ int main(int argc, char *argv[])
 	int ret = EXIT_FAILURE;
 	int c;
 
+	if (argc < 2)
+		usage();
+
 	set_logger_app_name("ksmbd.control");
 
 	if (getuid() != 0) {
-		pr_err("Please try it as root.\n");
+		pr_err("Please try running as root.\n");
 		return ret;
 	}
 
@@ -104,13 +117,13 @@ int main(int argc, char *argv[])
 	while ((c = getopt(argc, argv, "sd:cVh")) != EOF)
 		switch (c) {
 		case 's':
-			ksmbd_control_shutdown();
+			ret = ksmbd_control_shutdown();
 			break;
 		case 'd':
 			ret = ksmbd_control_debug(optarg);
 			break;
 		case 'c':
-			ret = ksmbd_control_show_version();
+			ksmbd_control_show_version();
 			break;
 		case 'V':
 			show_version();
@@ -120,9 +133,6 @@ int main(int argc, char *argv[])
 		default:
 			usage();
 	}
-
-	if (argc < 2)
-		usage();
 
 	return ret;
 }
