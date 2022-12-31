73576df0eb763c99ece87c6c1d17b7735d7a2111
Namjae Jeon <linkinjeon@kernel.org>
2022-12-31 17:10:32 +0900

    ksmbd-tools: add max connections parameter to global section
    Add max connections parameter to limit number of maximum simultaneous
    connections. The default value is 512. And the maximum value is 64k.
    Values greater than 64k or 0 will be silently set to 64k.
    
    Signed-off-by: Namjae Jeon <linkinjeon@kernel.org>

--- mountd/ipc.c
+++ mountd/ipc.c
@@ -175,6 +175,7 @@ static int ipc_ksmbd_starting_up(void)
 	ev->smb2_max_write = global_conf.smb2_max_write;
 	ev->smb2_max_trans = global_conf.smb2_max_trans;
 	ev->smbd_max_io_size = global_conf.smbd_max_io_size;
+	ev->max_connections = global_conf.max_connections;
 	ev->share_fake_fscaps = global_conf.share_fake_fscaps;
 	memcpy(ev->sub_auth, global_conf.gen_subauth, sizeof(ev->sub_auth));
 	ev->smb2_max_credits = global_conf.smb2_max_credits;
