cbb7a828949c28194df21f613af45325b82a4581
Atte Heikkilä <atteh.mailbox@gmail.com>
2023-10-03 19:05:34 +0300

    ksmbd-tools: reduce GLibby typing in session management
    Remove superfluous GLib types in session management by doing function
    pointer casts instead. Also, remove __free_func() by using
    g_list_free_full() instead.
    
    Signed-off-by: Atte Heikkilä <atteh.mailbox@gmail.com>
    Signed-off-by: Namjae Jeon <linkinjeon@kernel.org>

--- tools/management/session.c
+++ tools/management/session.c
@@ -18,18 +18,9 @@
 static GHashTable	*sessions_table;
 static GRWLock		sessions_table_lock;
 
-static void __free_func(gpointer data, gpointer user_data)
-{
-	struct ksmbd_tree_conn *tree_conn;
-
-	tree_conn = (struct ksmbd_tree_conn *)data;
-	tcm_tree_conn_free(tree_conn);
-}
-
 static void kill_ksmbd_session(struct ksmbd_session *sess)
 {
-	g_list_foreach(sess->tree_conns, __free_func, NULL);
-	g_list_free(sess->tree_conns);
+	g_list_free_full(sess->tree_conns, (GDestroyNotify)tcm_tree_conn_free);
 	g_rw_lock_clear(&sess->update_lock);
 	g_free(sess);
 }
@@ -173,14 +164,10 @@ int sm_check_sessions_capacity(unsigned long long id)
 	return ret;
 }
 
-static gint lookup_tree_conn(gconstpointer data, gconstpointer user_data)
+static int lookup_tree_conn(const struct ksmbd_tree_conn *tree_conn,
+			    const struct ksmbd_tree_conn *dummy)
 {
-	struct ksmbd_tree_conn *tree_conn = (struct ksmbd_tree_conn *)data;
-	struct ksmbd_tree_conn *dummy = (struct ksmbd_tree_conn *)user_data;
-
-	if (tree_conn->id == dummy->id)
-		return 0;
-	return 1;
+	return !(tree_conn->id == dummy->id);
 }
 
 int sm_handle_tree_disconnect(unsigned long long sess_id,
@@ -199,7 +186,7 @@ int sm_handle_tree_disconnect(unsigned long long sess_id,
 	dummy.id = tree_conn_id;
 	tc_list = g_list_find_custom(sess->tree_conns,
 				     &dummy,
-				     lookup_tree_conn);
+				     (GCompareFunc)lookup_tree_conn);
 	if (tc_list) {
 		struct ksmbd_tree_conn *tree_conn;
 
